273f2f6be4e5f809c0e2a16e32d960e7f5450a0e
chenshifang@jiuxunmu.com <chenshifang@jiuxunmu.com>
2022-10-25 14:41:58 +0800

    use redis save response

--- src/Middleware/DistributeTransact.php
+++ src/Middleware/DistributeTransact.php
@@ -5,6 +5,7 @@ namespace Laravel\ResetTransaction\Middleware;
 use Closure;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Response;
 use Laravel\ResetTransaction\Exception\RtException;
 use Laravel\ResetTransaction\Facades\RT;
@@ -14,21 +15,22 @@ class DistributeTransact
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $requestId = $request->header('rt-request-id');
         $transactId = $request->header('rt-transact-id');
-        
+
         if ($transactId) {
             if (!$requestId) {
                 throw new RtException('rt_request_id cannot be null');
             }
             session()->put('rt-request-id', $requestId);
-            $item = DB::connection('rt_center')->table('reset_transact_req')->where('request_id', $requestId)->first();
+//            $item = DB::connection('rt_center')->table('reset_transact_req')->where('request_id', $requestId)->first();
+            $item = Redis::get('RTCENTER:RESPONSE:' . $requestId);
             if ($item) {
                 $data = json_decode($item->response, true);
                 return Response::json($data);
@@ -44,6 +46,7 @@ class DistributeTransact
         $transactIdArr = explode('-', $transactId);
         if ($transactId && $response->isSuccessful()) {
             RT::middlewareRollback();
+            Redis::setex('RTCENTER:RESPONSE:' . $requestId, 5 * 60, $response->getContent());
             DB::connection('rt_center')->table('reset_transact_req')->insert([
                 'transact_id' => $transactIdArr[0],
                 'request_id' => $requestId,
